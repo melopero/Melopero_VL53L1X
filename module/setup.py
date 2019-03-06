f5dd4b380e266224ec211c6c38b533f665dc0c1c
leoli51 <46094699+leoli51@users.noreply.github.com>
2019-03-06 10:39:51 +0100

    new version in setup + added MANIFEST.in for .h files

--- module/setup.py
+++ module/setup.py
@@ -4,7 +4,7 @@ import setuptools
 import os
 
 sources_files = []
-lib_dirs = ['.','api/core', 'api/platform', 'api/python-interface']
+lib_dirs = ['api/core', 'api/platform', 'api/python-interface']
 for libdir in lib_dirs:
     for file in os.listdir(os.path.join(os.getcwd(), libdir)):
         if file.endswith('.c'):
@@ -15,18 +15,20 @@ extension = setuptools.Extension(
     'vl53l1x_api',
     define_macros=[],
     include_dirs=lib_dirs,
+    #extra_compile_args=['-std=c99'],
     libraries=[],
     library_dirs=[],
     sources= sources_files)
 
 setuptools.setup(
     name="melopero_vl53l1x",
-    version="0.1.0",
+    version="0.2.0",
     description="A module to easily access Melopero's VL53L1X sensor's features",
     url="https://github.com/melopero/Melopero_VL53L1X/tree/master/module",
     author="Melopero",
     author_email="info@melopero.com",
     license="MIT",
+    ext_modules=[extension],
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -34,5 +36,4 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
     ],
-    install_requires=["smbus2"],
 )
